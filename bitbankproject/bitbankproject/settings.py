bc09d6fc3ef6d3823885b08f3a8153337cc65f58
shu <shusaku.ishikawa@unisys.co.jp>
2019-04-13 11:42:18 +0900

    a

--- bitbankproject/bitbankproject/settings.py
+++ bitbankproject/bitbankproject/settings.py
@@ -222,7 +222,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static_dev/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bitbank/static'),
 ]
