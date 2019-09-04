5cc0c1a09d9f182f0fdc321ec7734c80c3f622d4
shu <shusaku.ishikawa@unisys.co.jp>
2019-09-04 20:42:39 +0900

    a

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,5 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static_v3/'
 
-STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
